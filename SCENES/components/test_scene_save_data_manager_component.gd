981bb04bebdab00dcc32bd2d3914eeada9aa83cb
ChyRoseBun <chyrose1927@gmail.com>
2025-05-02 14:03:43 -0400

    savedatapart2

--- SCENES/components/test_scene_save_data_manager_component.gd
+++ SCENES/components/test_scene_save_data_manager_component.gd
@@ -2,7 +2,7 @@ class_name TestSceneSaveDataManagerComponent
 extends Node
 
 func _ready() -> void:
-	pass
+	call_deferred("load_test_scene")
 
 func load_test_scene():
 	SaveGameManager.load_game()
